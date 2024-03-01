d1386e339d8879c5c6e18c1d404038ff7b56eae4
Labonya <1aklimakter@gmail.com>
2024-03-01 06:24:20 +0600

    update category

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,6 @@ import OTP from './Components/OTPs.jsx';
 import PasswordSubmit from './Components/PasswordSubmit.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthContext from './Context/AuthContext.jsx';
-import EditForm from './Shared/EditForm.jsx';
 
 
 const router = createBrowserRouter([
@@ -85,10 +84,7 @@ const router = createBrowserRouter([
         path: '/dashboard/riddle',
         element: <Riddles />
       },
-      {
-        path:"/dashboard/edit",
-        element:<EditForm/>
-      }
+     
     ]
   },
 ])
